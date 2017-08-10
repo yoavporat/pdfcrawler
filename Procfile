b8c13edc683394e728dc3f6504d67b3d9816519b
Yoav Porat <yoavporat@hotmail.com>
2017-08-10 13:24:55 +0300

    stracture change

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: cd crawler; gunicorn crawler.wsgi --log-file -
+web: python manage.py runserver 0.0.0.0:8000
--- crawler/pdfapp/admin.py
+++
@@ -1,5 +0,0 @@
-from django.contrib import admin
-import models
-
-admin.site.register(models.Doc)
-admin.site.register(models.Urls)
